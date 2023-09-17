1d822a30e156471af719fe8e78df06962d4856d8
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-09-17 12:57:18 +0530

    till display-section and top-btn

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function DisplaySection() {
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left:0,
+      behavior: "smooth",
+    });
+  
+  }
+
   return (
     <div className="display-section wrapper">
       <h2 className="title">New</h2>
@@ -9,7 +19,7 @@ function DisplaySection() {
         A display that's upto 2x brighter in the sun.
       </span>
       <button className="button">Try me!</button>
-      <button className="button">TOP</button>
+      <button className="button" onClick={handleScrollToTop}>TOP</button>
     </div>
   );
 }
