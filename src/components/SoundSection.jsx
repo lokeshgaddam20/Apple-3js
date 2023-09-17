b5521d007e475f1a7fa86d909f525b5053b2ab5d
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-09-17 12:50:33 +0530

    sound-display scroll

--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 function SoundSection() {
+
+    const handleLearnMore = () => {
+        const element = document.querySelector('.display-section');
+        window.scrollTo({
+            top: element.getBoundingClientRect().bottom,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
     return (
         <div className="sound-section wrapper">
             <div className="body">
@@ -15,7 +25,7 @@ function SoundSection() {
                             <button className="button">Buy</button>
                         </li>
                         <li>
-                            <a href="" className="link">Learn more.</a>
+                            <a href="" className="link" onClick={handleLearnMore}>Learn more.</a>
                         </li>
                     </ul>
                 </div>
